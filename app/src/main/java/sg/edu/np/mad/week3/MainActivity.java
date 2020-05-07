3e6291db58f97c1a91c0b73f395d44633560f04d
Qi Xun <qixunnn@gmail.com>
2020-05-07 23:46:39 +0800

    Completed Practical 3

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -2,13 +2,36 @@ package sg.edu.np.mad.week3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer media;
+    VideoView view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Load Audio
+        media = MediaPlayer.create(this,R.raw.baby_laughing);
+        media.start();
+
+        //Load View
+        view = findViewById(R.id.videoView);
+        view.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.rabbid));
+        view.start();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        media.release();
+        media = null;
     }
 }
